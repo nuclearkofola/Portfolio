f83347924621202e1ff61998106f791d7eae0ad2
Stanislav Svoboda <52444330+nuclearkofola@users.noreply.github.com>
2025-10-24 16:55:16 +0200

    react update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
 const App = () => {
   return (
-<main>
-<div>
-    <h1 class="text-3xl font-bold underline">Welcome to my Portfolio!</h1>
-</div>
-</main>
+    <main>
+      <div>
+        <h1 className="text-3xl font-bold underline">Welcome to my Portfolio!</h1>
+      </div>
+    </main>
   )
 }
 
 export default App
+
